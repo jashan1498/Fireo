a114c899bd43226cbf23a00f0ce600cd0512b0bc
jashan <jashan1498@gmail.com>
2020-05-16 19:37:14 +0530

    missing files added

--- app/src/main/java/com/example/fireo/activities/ProfileSettingsActivity.java
+++ app/src/main/java/com/example/fireo/activities/ProfileSettingsActivity.java
@@ -1,15 +1,20 @@
 package com.example.fireo.activities;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.databinding.DataBindingUtil;
 
 import com.example.fireo.BaseApplication;
 import com.example.fireo.R;
+import com.example.fireo.Utils.LoginHelper;
 import com.example.fireo.databinding.ActivityProfileSettingsBinding;
 import com.example.fireo.presenter.ProfileSettingsPresenter;
 
 public class ProfileSettingsActivity extends BaseApplication implements ProfileSettingsPresenter.View {
+    ProfileSettingsPresenter presenter;
+    EditText emailText, phoneNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,12 +25,27 @@ public class ProfileSettingsActivity extends BaseApplication implements ProfileS
         if (user != null) {
             binding.setUser(user);
         }
+        presenter = new ProfileSettingsPresenter(this);
     }
 
     @Override
     public void init() {
-
+        emailText = findViewById(R.id.email_edit_text);
+        phoneNumber = findViewById(R.id.phone_number_edit_text);
+        findViewById(R.id.save_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validate();
+            }
+        });
     }
+
+    private void validate() {
+        if (emailText.getText().length() >= 5 && phoneNumber.getText().length() >= 6 && LoginHelper.validateEmail(emailText.getText().toString())) {
+            presenter.updateFirebaseUser(emailText.getText().toString(), phoneNumber.getText().toString());
+        }
+    }
+
 
     @Override
     public void setData() {
